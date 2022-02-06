b7101765f2582797b41d8b708a18f928794f06d9
hitmaizer <jose.sousalves@gmail.com>
2022-02-06 13:59:46 +0000

    styling primary and outline

--- src/elements/ButtonStyles.js
+++ src/elements/ButtonStyles.js
@@ -1,17 +1,26 @@
 import styled, { css } from 'styled-components';
 
 export default styled.button`
-  border-radius: 12px;
+  border-radius: 8px;
   font-family: ${(props) => props.theme.fonts.montserrat};
   font-weight: 500;
   font-size: 18px;
   line-height: 22px;
-  padding: 16px 40px;
+  padding: 12px 32px;
+  cursor: pointer;
+  transition: all 1ms ease-out;
+  
+  &:hover {
+      transform: translateY(-3%);
+  }
 
   ${(props) => props.primary && css `
     border: none;
     color: ${props.theme.colors.whiteCardBg};
     background-color: ${props.theme.colors.primaryBlue};
+    &:hover {
+        background-color: ${props.theme.colors.darkenBlue};
+    }
 
   `}
 
@@ -19,6 +28,12 @@ export default styled.button`
     border: 2px solid ${props.theme.colors.primaryBlue};
     background: transparent;
     color: ${props.theme.colors.primaryBlue};
+    
+    &:hover {
+        background: ${props.theme.colors.primaryBlue};
+        color: ${props.theme.colors.whiteCardBg};
+    }
+  
   `}
 
 
