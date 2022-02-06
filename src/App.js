359cef0c13a4d6bc39f58fa03b2e264b0c6af0fd
hitmaizer <jose.sousalves@gmail.com>
2022-02-06 15:25:03 +0000

    app markup for card styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import Button from "./components/Button"
 import Theme from "./theme/Theme"
-
+import Card from "./components/Card"
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default function App() {
     return (
-        <ThemeProvider theme={Theme}>
-            <Button />
-        </ThemeProvider>
+        <Router>
+            <div className="page__wrapper">
+                <ThemeProvider theme={Theme}>
+                    <Card />
+                    
+                </ThemeProvider>
+            </div>
+        </Router>
     )
 }
