158e47dd674c4b4613a9d04a3e5cd6f052a9ffc2
hitmaizer <jose.sousalves@gmail.com>
2022-02-06 21:36:19 +0000

    filter component jsx markup

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -5,17 +5,15 @@ import Btn from "../elements/ButtonStyles"
 export default function Filter(props) {
 
 
-
-
     return (
         <Card>
             <div className="filter__card flex-col">
                 <h3 className="filter__title">Filter projects by category</h3>
                 <div className="filter__btns flex-row">
-                    <Btn primary>Responsive</Btn> 
-                    <Btn outline>React</Btn>
-                    <Btn outline>UI</Btn>
-                    <Btn outline>Other</Btn>
+                    <Btn filter>Responsive</Btn> 
+                    <Btn filter>React</Btn>
+                    <Btn filter>UI Design</Btn>
+                    <Btn filter>Other</Btn>
                 </div>
             </div>
         </Card>
