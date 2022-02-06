c6e35f54a8dfd6b988995cbc4535f6e393f9b9d8
hitmaizer <jose.sousalves@gmail.com>
2022-02-06 12:19:05 +0000

    set up the initial app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import Theme from "./theme/Theme"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+    return (
+        <ThemeProvider theme={Theme}>
+            <h1>Hello mate!</h1>
+        </ThemeProvider>
+    )
 }
-
-export default App;
