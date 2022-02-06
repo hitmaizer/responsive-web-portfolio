338051904af9b14ece27c9dcce97f5ccd2f3379d
hitmaizer <jose.sousalves@gmail.com>
2022-02-06 12:38:19 +0000

    added some styling variables to theme obj

--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -1,5 +1,21 @@
 const Theme = {
+    colors: {
+        primaryBlue: "#2F80ED",
+        offWhiteBg: "#F2F2F2",
+        whiteCardBg: "#FFFFFF",
+        darkGray: "#4F4F4F",
+        lightGray: "#828282",
+        darkerGray: "#333333",
+        black: "#000000",
+    },
 
+    fonts: {
+        montserrat: "'Montserrat', sans-serif",
+    },
+
+    dropShadow: {
+        dropShadow: "0px 10px 30px rgba(51, 51, 51, 0.1);"
+    }
 }
 
 export default Theme
