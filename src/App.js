a2a4fd8057e374228ad84a33fa0fb5112d95bbc0
hitmaizer <jose.sousalves@gmail.com>
2022-02-06 16:01:28 +0000

    intro section jsx markup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import Theme from "./theme/Theme"
-import Card from "./components/Card"
+import WorksCard from "./components/WorksCard"
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Intro from "./components/Intro"
 
 export default function App() {
     return (
         <Router>
             <div className="page__wrapper">
                 <ThemeProvider theme={Theme}>
-                    <Card />
-                    
+                    <div className="page__grid">
+                        <div className="intro__section">
+                            <Intro />
+                        </div>
+                        <WorksCard />
+                    </div>
                 </ThemeProvider>
             </div>
         </Router>
