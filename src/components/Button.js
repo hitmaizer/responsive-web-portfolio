83094f8031c73c97477c7296a79cd82ea5d014b8
hitmaizer <jose.sousalves@gmail.com>
2022-02-06 13:10:50 +0000

    setting up button component

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Btn from "../elements/ButtonStyles"
+
+
+
+
+const Button = (props) => {
+    return (
+        <Btn outline>Hello mate!</Btn>
+    )
+}
+
+export default Button
