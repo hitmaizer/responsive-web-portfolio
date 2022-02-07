7879330e5a8d0f677b1db2a6ad12bfd78a674df5
hitmaizer <jose.sousalves@gmail.com>
2022-02-07 20:42:51 +0000

    added the data for the work component

---
+++ src/data.js
@@ -0,0 +1,102 @@
+export default {
+    "works": [
+        {
+            "id": "1",
+            "title": "Page 404",
+            "img": "images/works/page404.png",
+            "tags": ["responsive", "HTML", "CSS"],
+            "category": "responsive",
+            "description": "A simple page 404, as my submission for the devChallenges.io #1",
+            "demo": "https://page-404-one.vercel.app/",
+            "code": "https://github.com/hitmaizer/responsive-web-404",
+        },
+        
+        {
+            "id": "2",
+            "title": "Edie's Homepage",
+            "img": "images/works/ediepage.png",
+            "tags": ["responsive", "HTML", "CSS"],
+            "category": "responsive",
+            "description": "The markup was done in an HTML file, the styling was done on SCSS partials for each section of the page. Most of the layout was made with Flexbox with the exception of the mosaic layouts which were implemented with CSS grid. There's also a bit of JavaScript for the hamburger menu to toggle a class that opens the menu, and also an Intersection Observer which removes the class on scrolling. A couple of fun little CSS animations for links hover states and also for the hamburger menu opening.",
+            "demo": "https://edie-homepage-hitmaizer.vercel.app/",
+            "code": "https://github.com/hitmaizer/edie-homepage",
+        },
+        
+        {
+            "id": "3",
+            "title": "Checkout Page",
+            "img": "images/works/checkout.png",
+            "tags": ["responsive", "HTML", "CSS"],
+            "category": "responsive",
+            "description": "For this project, I started by doing all the markup on my HTML file, created all my SCSS partials for the styling, and used Flexbox for the layout of my elements. Used a tiny bit of JavaScript to implement the increment/decrement functionality of the number of items on the shopping cart!",
+            "demo": "https://responsive-checkout-hitmaizer.vercel.app/",
+            "code": "https://github.com/hitmaizer/responsive-checkout",
+        },
+        
+        {
+            "id": "4",
+            "title": "Button Components",
+            "img": "images/works/buttons.png",
+            "tags": ["React", "JavaScript", "Styled Components"],
+            "category": "react",
+            "description": "Several different renders of different states of the same button component",
+            "demo": "https://btn-components-hitmaizer.vercel.app/",
+            "code": "https://github.com/hitmaizer/btn-styledcomponents",
+        },
+        
+        {
+            "id": "5",
+            "title": "Quizz Game",
+            "img": "images/works/quizz.png",
+            "tags": ["React", "JavaScript", "SCSS"],
+            "category": "react",
+            "description": "A simple quizz game. Fetches 4 random questions from an API and then checks the results!",
+            "demo": "https://quizz-react.vercel.app/",
+            "code": "https://github.com/hitmaizer/quizz-project",
+        },
+        
+        {
+            "id": "6",
+            "title": "Tenzies Game",
+            "img": "images/works/tenzies.png",
+            "tags": ["React", "JavaScript", "SCSS"],
+            "category": "react",
+            "description": "Simple Tenzies game developed as introduction to React. Part of a Scrimba course",
+            "demo": "https://tenzies-nine.vercel.app/",
+            "code": "https://github.com/hitmaizer/tenzies",
+        },
+        
+        {
+            "id": "7",
+            "title": "Car Dashboard",
+            "img": "images/works/carui.jpg",
+            "tags": ["UI Design", "Figma", "UI/UX"],
+            "category": "ui",
+            "description": "Car Dashboard UI design as part of the DailyUI Challenge",
+            "demo": "https://dribbble.com/shots/15440795-Car-Interface",
+            "code": "",
+        },
+        
+        {
+            "id": "8",
+            "title": "SocialN UI/UX",
+            "img": "images/works/socialN.png",
+            "tags": ["UI Design", "Figma", "UI/UX"],
+            "category": "ui",
+            "description": "UI/UX Design for a social network App concept",
+            "demo": "",
+            "code": "",
+        },
+        
+        {
+            "id": "9",
+            "title": "Job Listing",
+            "img": "images/works/joblisting.png",
+            "tags": ["UI Design", "Figma", "UI/UX"],
+            "category": "ui",
+            "description": "A job listing app, designed on Figma ",
+            "demo": "",
+            "code": "",
+        },
+    ]
+}
