697aa080d97887e7229f307121a8121ac30d23bb
hitmaizer <jose.sousalves@gmail.com>
2022-02-07 11:35:20 +0000

    added markup portfolio + socials components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Theme from "./theme/Theme"
-import Intro from "./components/Intro"
+import Theme from "./theme/Theme";
+import Intro from "./components/Intro";
 import Skills from './components/Skills';
-import Certificates from "./components/Certificates"
-import Hobbies from "./components/Hobbies"
-import Works from "./components/Works"
+import Certificates from "./components/Certificates";
+import Hobbies from "./components/Hobbies";
+import Works from "./components/Works";
 import Filter from './components/Filter';
+import Portfolio from './components/Portfolio';
+import Socials from './components/Socials';
 
 export default function App() {
     return (
@@ -18,14 +20,20 @@ export default function App() {
                         <div className="intro__section">
                             <Intro />
                         </div>
+                        <div className="socials__section">
+                            <Socials />
+                        </div>
                         <div className="skills__section">
                             <Skills />
                         </div>
-                        <div className="certificates__section">
-                            <Certificates />
+                        <div className="portfolio__section">
+                            <Portfolio />
                         </div>
                         <div className="hobbies__section">
                             <Hobbies />
+                        </div>
+                        <div className="certificates__section">
+                            <Certificates />
                         </div>
                         <div className="filter__section">
                             <Filter />
