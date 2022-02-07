a0bdcc544069065215fa5d1d352e4333d9be0821
hitmaizer <jose.sousalves@gmail.com>
2022-02-07 22:33:15 +0000

    -hard core +render from data

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,15 +1,36 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import WorksCard from "./WorksCard";
+import Data from "../data"
 
 
 export default function Works(props) {
-    return (
-        
-            <div className="works__card flex-row">
-                <WorksCard />
-                <WorksCard />
-                <WorksCard />
-            </div>
+
+   const [worksData, setWorksData] = React.useState([])
+   const currentState = props.state
     
+   React.useEffect(() => {
+        const worksArray = Data.works
+        setWorksData(worksArray)
+    }, [])
+
+    const worksElements = worksData.map(work => (
+    
+    <WorksCard 
+    key={work.id}
+    image={work.img}
+    tags={work.tags}
+    title={work.title}
+    description={work.description}
+    demo={work.demo}
+    code={work.code}
+    />
+    ))
+   
+    return (
+        <div className="works__card flex-row">
+            {worksElements}
+        </div>
     )
 }
