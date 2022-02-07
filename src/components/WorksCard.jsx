4c23dc2d09b7e4d6712032e1a079d27ef78e5f02
hitmaizer <jose.sousalves@gmail.com>
2022-02-07 22:34:16 +0000

    -hard code +render from data

--- src/components/WorksCard.jsx
+++ src/components/WorksCard.jsx
@@ -1,30 +1,30 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import CardStyled from "../elements/CardStyles"
 import Btn from "../elements/ButtonStyles"
 
 
 export default function WorksCard(props) {
-
+    
 
     return (
         <div className="works__card">
             <CardStyled >
-                <img src="images/recipe-page.png" alt="" className="card__img" />
+                <img src={`http://localhost:3002/images/works/${props.image}`} alt="" className="card__img" />
                 <div className="card__tags flex-row">
-                    <p className="card__tags--item">#HTML</p>
-                    <p className="card__tags--item">#CSS</p>
-                    <p className="card__tags--item">#responsive</p>
+                    <p className="card__tags--item">#{props.tags[0]}</p>
+                    <p className="card__tags--item">#{props.tags[1]}</p>
+                    <p className="card__tags--item">#{props.tags[2]}</p>
                 </div>
-                <h1 className="card__title">Recipe Blog</h1>
-                <p className="card__description">In this project, 
-                I work with HTML and CSS to create a responsive page. 
-                This page is similiar with a page. The design is from devchallenge.io</p>
+                <h1 className="card__title">{props.title}</h1>
+                <p className="card__description">{props.description}</p>
                 <div className="card__btns flex-row">
                     <Btn primary>
-                        <a className="card__link" href="https://edie-homepage-hitmaizer.vercel.app/" target="_blank" rel="noreferrer">Demo</a>
+                        <a className="card__link" href={props.demo} target="_blank" rel="noreferrer">Demo</a>
                     </Btn>
                     <Btn outline>
-                        <a className="card__link" href="https://edie-homepage-hitmaizer.vercel.app/" target="_blank" rel="noreferrer">Code</a>
+                        <a className="card__link" href={props.code} target="_blank" rel="noreferrer">Code</a>
                     </Btn>
                     
                 </div>
