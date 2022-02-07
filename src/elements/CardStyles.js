c9462978d062e81b018b7449845d347917f11d98
hitmaizer <jose.sousalves@gmail.com>
2022-02-07 17:46:22 +0000

    adapted for darkmode

--- src/elements/CardStyles.js
+++ src/elements/CardStyles.js
@@ -1,19 +1,20 @@
 import styled from 'styled-components';
 
+
 export default styled.div`
     gap: 16px;
     padding: 24px;
-    background-color: ${(props) => props.theme.colors.whiteCardBg};
-    font-family: ${(props) => props.theme.fonts.montserrat};
-    
+    width: auto;
     border-radius: 12px;
     cursor: pointer;
     transition: transform 0.2s ease;
-    
+    background-color: ${(props) => props.theme.cardBg};
     
     &:hover {
-        box-shadow: 0px 10px 30px rgba(51, 51, 51, 0.1);
+        box-shadow: ${(props) => props.theme.dropShadow};
         transform: translateY(-5%);
     }
+
+    
 
 `
