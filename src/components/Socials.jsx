b212edecb82b066fc4839df58b27473b95742e60
hitmaizer <jose.sousalves@gmail.com>
2022-02-07 14:10:34 +0000

    socials component markup

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Twitter, Linkedin, Dribbble, Github } from '@styled-icons/boxicons-logos';
+import { Sun } from '@styled-icons/boxicons-regular/Sun';
 import Card from '../elements/CardStyles';
+
 
 export default function Socials() {
     return (
@@ -8,11 +10,35 @@ export default function Socials() {
             <div className="socials__card">
                 <h3 className="socials__title">Follow me on my socials!</h3>
                 <div className="socials__icons flex-row">
-                    <Linkedin size="40px" />
-                    <Twitter size="40px" />
-                    <Github size="40px" />
-                    <Dribbble size="40px" />
+                    <div className="icons__item flex-col">
+                        <a href="https://www.linkedin.com/in/hitmaizer/">
+                            <Linkedin size="40px" className="linkedin" />
+                        </a>
+                    </div>
+                    <div className="icons__item flex-col">
+                        <a href="https://twitter.com/josesousalves">
+                            <Twitter size="40px" className="twitter" />
+                        </a>
+                    </div>
+                    <div className="icons__item flex-col">
+                        <a href="https://github.com/hitmaizer">
+                            <Github size="40px" className="github" />
+                        </a>
+                    </div>
+                    <div className="icons__item flex-col">
+                        <a href="https://dribbble.com/hitmaizer">
+                            <Dribbble size="40px" className="dribbble" />
+                        </a>
+                    </div>
                 </div>
+            <div className="toggle__container">
+                <div className="toggle">
+                    <div className="toggle__btn" >
+                        <Sun size="24px" className="btn__icon" />
+                    </div>
+                </div>
+                    <p className="toggle__text">Light</p>
+            </div>
             </div>
         </Card>
     )
