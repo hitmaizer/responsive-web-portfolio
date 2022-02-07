e1019a05afe8f26ac797ae6b71f90b38589ef9b9
hitmaizer <jose.sousalves@gmail.com>
2022-02-07 22:33:29 +0000

    filter buttons change state

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import Card from "../elements/CardStyles"
 import Btn from "../elements/ButtonStyles"
@@ -10,10 +12,10 @@ export default function Filter(props) {
             <div className="filter__card flex-col">
                 <h3 className="filter__title">Filter projects by category</h3>
                 <div className="filter__btns flex-row">
-                    <Btn filterBtn>Responsive</Btn> 
-                    <Btn filterBtn>React</Btn>
-                    <Btn filterBtn>UI Design</Btn>
-                    <Btn filterBtn>Other</Btn>
+                    <Btn filterBtn onClick={() => props.stateChanger(1)}>Responsive</Btn> 
+                    <Btn filterBtn onClick={() => props.stateChanger(2)}>React</Btn>
+                    <Btn filterBtn onClick={() => props.stateChanger(3)}>UI Design</Btn>
+                    <Btn filterBtn onClick={() => props.stateChanger(4)}>Other</Btn>
                 </div>
             </div>
         </Card>
