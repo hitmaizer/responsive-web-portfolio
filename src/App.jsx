53af9890b6a6ececef932c46087df8985b44b4b3
hitmaizer <jose.sousalves@gmail.com>
2022-02-07 09:51:58 +0000

    set up ESLint + minor fixes to correct errors

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Theme from "./theme/Theme"
+import Intro from "./components/Intro"
+import Skills from './components/Skills';
+import Certificates from "./components/Certificates"
+import Hobbies from "./components/Hobbies"
+import Works from "./components/Works"
+import Filter from './components/Filter';
+
+export default function App() {
+    return (
+        <Router>
+            <div className="page__wrapper">
+                <ThemeProvider theme={Theme}>
+                    <div className="page__grid">
+                        <div className="intro__section">
+                            <Intro />
+                        </div>
+                        <div className="skills__section">
+                            <Skills />
+                        </div>
+                        <div className="certificates__section">
+                            <Certificates />
+                        </div>
+                        <div className="hobbies__section">
+                            <Hobbies />
+                        </div>
+                        <div className="filter__section">
+                            <Filter />
+                        </div>
+                        <div className="works__section">
+                            <Works />
+                        </div>
+                    </div>
+                </ThemeProvider>
+            </div>
+        </Router>
+    )
+}
