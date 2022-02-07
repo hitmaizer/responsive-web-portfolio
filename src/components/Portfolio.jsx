6aec9d5b0e7ba2d2f041ecc469b31e6b17c5a110
hitmaizer <jose.sousalves@gmail.com>
2022-02-07 11:36:10 +0000

    markup for the portfolio component

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from '../elements/CardStyles'
+
+export default function Portfolio() {
+    return (
+        <div className="portfolio__card">
+            <Card>
+                <h5 className="portfolio__category">Blog</h5>
+                <div className="portfolio__midsection flex-col">
+                    <h3 className="portfolio__title">A bit more info about this page!</h3>
+                    <img src="images/frontend.jpg" alt="" className="portfolio__img" />
+                </div>
+                <p className="portfolio__text">This page was originally thought as part of <b>devChallenges.io</b>&apos;s 
+                last <b>&quot;Responsive Web Design&quot;</b> path challenge, which requires participants to develop a portfolio.<br />
+                <br />
+                This one was made with <b>React</b>, and the styling was done with <b>Styled Components</b> and <b>SCSS</b>. 
+                This way I was able to showcase all my skills as a Front End Developer.</p>
+            </Card>
+        </div>
+    )
+}
