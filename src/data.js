72930114909f3b1096afc87c534fa535ac227609
hitmaizer <jose.sousalves@gmail.com>
2022-02-07 22:34:29 +0000

    works data array of objects

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export default {
         {
             "id": "1",
             "title": "Page 404",
-            "img": "images/works/page404.png",
+            "img": "page404.png",
             "tags": ["responsive", "HTML", "CSS"],
             "category": "responsive",
             "description": "A simple page 404, as my submission for the devChallenges.io #1",
@@ -14,7 +14,7 @@ export default {
         {
             "id": "2",
             "title": "Edie's Homepage",
-            "img": "images/works/ediepage.png",
+            "img": "ediepage.png",
             "tags": ["responsive", "HTML", "CSS"],
             "category": "responsive",
             "description": "The markup was done in an HTML file, the styling was done on SCSS partials for each section of the page. Most of the layout was made with Flexbox with the exception of the mosaic layouts which were implemented with CSS grid. There's also a bit of JavaScript for the hamburger menu to toggle a class that opens the menu, and also an Intersection Observer which removes the class on scrolling. A couple of fun little CSS animations for links hover states and also for the hamburger menu opening.",
@@ -25,7 +25,7 @@ export default {
         {
             "id": "3",
             "title": "Checkout Page",
-            "img": "images/works/checkout.png",
+            "img": "checkout.png",
             "tags": ["responsive", "HTML", "CSS"],
             "category": "responsive",
             "description": "For this project, I started by doing all the markup on my HTML file, created all my SCSS partials for the styling, and used Flexbox for the layout of my elements. Used a tiny bit of JavaScript to implement the increment/decrement functionality of the number of items on the shopping cart!",
@@ -36,7 +36,7 @@ export default {
         {
             "id": "4",
             "title": "Button Components",
-            "img": "images/works/buttons.png",
+            "img": "buttons.png",
             "tags": ["React", "JavaScript", "Styled Components"],
             "category": "react",
             "description": "Several different renders of different states of the same button component",
@@ -47,7 +47,7 @@ export default {
         {
             "id": "5",
             "title": "Quizz Game",
-            "img": "images/works/quizz.png",
+            "img": "quizz.png",
             "tags": ["React", "JavaScript", "SCSS"],
             "category": "react",
             "description": "A simple quizz game. Fetches 4 random questions from an API and then checks the results!",
@@ -58,7 +58,7 @@ export default {
         {
             "id": "6",
             "title": "Tenzies Game",
-            "img": "images/works/tenzies.png",
+            "img": "tenzies.png",
             "tags": ["React", "JavaScript", "SCSS"],
             "category": "react",
             "description": "Simple Tenzies game developed as introduction to React. Part of a Scrimba course",
@@ -69,7 +69,7 @@ export default {
         {
             "id": "7",
             "title": "Car Dashboard",
-            "img": "images/works/carui.jpg",
+            "img": "carui.jpg",
             "tags": ["UI Design", "Figma", "UI/UX"],
             "category": "ui",
             "description": "Car Dashboard UI design as part of the DailyUI Challenge",
@@ -80,7 +80,7 @@ export default {
         {
             "id": "8",
             "title": "SocialN UI/UX",
-            "img": "images/works/socialN.png",
+            "img": "socialN.png",
             "tags": ["UI Design", "Figma", "UI/UX"],
             "category": "ui",
             "description": "UI/UX Design for a social network App concept",
@@ -91,7 +91,7 @@ export default {
         {
             "id": "9",
             "title": "Job Listing",
-            "img": "images/works/joblisting.png",
+            "img": "joblisting.jpg",
             "tags": ["UI Design", "Figma", "UI/UX"],
             "category": "ui",
             "description": "A job listing app, designed on Figma ",
