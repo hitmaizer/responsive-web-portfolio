cfbe02817a61f32dc2b8a3bbe3076cdb75b013d7
hitmaizer <jose.sousalves@gmail.com>
2022-02-09 16:25:44 +0000

    switch img for svgs + conditional render

--- src/components/Hobbies.jsx
+++ src/components/Hobbies.jsx
@@ -1,23 +1,27 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import Card from "../elements/CardStyles"
 
-export default function Hobbies() {
+export default function Hobbies({darkMode}) {
     return (
         <Card>
             <div className="hobbies__card flex-col">
                 <h3 className="hobbies__title">Hobbies</h3>
-                <div className="hobbie__card">
-                    <img src="images/retro.jpg" alt="" className="hobbie__img" />
+                <div className="hobbie__card flex-col">
+                    {darkMode === "light" && <img src="images/gaming-light.svg" alt="" className="hobbie__img" />}
+                    {darkMode === "dark" && <img src="images/gaming-dark.svg" alt="" className="hobbie__img" />}
                     <h6 className="hobbie__title">Gaming</h6>
                     <p className="hobbie__text">Certified videogamer since the 90&apos;s.</p>
                 </div>
-                <div className="hobbie__card">
-                    <img src="images/sports.jpg" alt="" className="hobbie__img" />
+                <div className="hobbie__card flex-col">
+                    {darkMode === "light" && <img src="images/sports-light.svg" alt="" className="hobbie__img" />}
+                    {darkMode === "dark" && <img src="images/sports-dark.svg" alt="" className="hobbie__img" />}
                     <h6 className="hobbie__title">Sports</h6>
                     <p className="hobbie__text">Sports lover. Love watching pretty much every sport!</p>
                 </div>
-                <div className="hobbie__card">
-                    <img src="images/watching.jpg" alt="" className="hobbie__img" />
+                <div className="hobbie__card flex-col">
+                    {darkMode === "light" && <img src="images/watching-light.svg" alt="" className="hobbie__img" />}
+                    {darkMode === "dark" && <img src="images/watching-dark.svg" alt="" className="hobbie__img" />}
                     <h6 className="hobbie__title">Series and Movies</h6>
                     <p className="hobbie__text">Watching series and movies is one of the best ways to rest the mind.</p>
                 </div>
