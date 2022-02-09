7a9608917f6d26521c335cd8d67c481f132b4cc6
hitmaizer <jose.sousalves@gmail.com>
2022-02-09 13:55:01 +0000

    conditional rendering of img

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,14 +1,16 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import Card from '../elements/CardStyles'
 
-export default function Portfolio() {
+export default function Portfolio({ darkMode }) {
     return (
         <div className="portfolio__card">
             <Card>
                 <h5 className="portfolio__category">Blog</h5>
                 <div className="portfolio__midsection flex-col">
                     <h3 className="portfolio__title">A bit more info about this page!</h3>
-                    <img src="images/frontend.jpg" alt="" className="portfolio__img" />
+                    {darkMode === "light" && <img src="images/dev-light.svg" alt="" className="portfolio__img" />}
+                    {darkMode === "dark" && <img src="images/dev-dark.svg" alt="" className="portfolio__img" />}  
                 <p className="portfolio__text">This page was originally thought as part of <b>devChallenges.io</b>&apos;s 
                 last <b>&quot;Responsive Web Design&quot;</b> path challenge, which requires participants to develop a portfolio.<br />
                 <br />
