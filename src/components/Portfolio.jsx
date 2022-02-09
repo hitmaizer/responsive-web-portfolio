80e5a207bf8490b7b93634cc043269664eb721b1
hitmaizer <jose.sousalves@gmail.com>
2022-02-09 16:26:23 +0000

    adjusted text formatting on portfolio component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,11 +11,10 @@ export default function Portfolio({ darkMode }) {
                     <h3 className="portfolio__title">A bit more info about this page!</h3>
                     {darkMode === "light" && <img src="images/dev-light.svg" alt="" className="portfolio__img" />}
                     {darkMode === "dark" && <img src="images/dev-dark.svg" alt="" className="portfolio__img" />}  
-                <p className="portfolio__text">This page was originally thought as part of <b>devChallenges.io</b>&apos;s 
-                last <b>&quot;Responsive Web Design&quot;</b> path challenge, which requires participants to develop a portfolio.<br />
+                <p className="portfolio__text">This page was originally thought as part of <span className="important__word"><b>devChallenges.io&apos;s</b></span> last <span className="important__word"><b>&quot;Responsive Web Developer&quot;</b></span> path challenge, which requires participants to develop a portfolio.<br />
                 <br />
-                This one was made with <b>React</b>, and the styling was done with <b>Styled Components</b> and <b>SCSS</b>. 
-                This way I was able to showcase all my skills as a Front End Developer.</p>
+                This one was made with <span className="important__word"><b>React</b></span>, and the styling was done with <span className="important__word"><b>Styled Components</b></span> and <span className="important__word"><b>SCSS</b></span>. 
+                This way I was able to showcase all my skills as a <span className="important__word"><b>Front End Developer.</b></span></p>
                 </div>
             </Card>
         </div>
