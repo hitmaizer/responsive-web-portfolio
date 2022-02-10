302b3d704fa8b310f06d9d4cc7b3c4da49a96f70
hitmaizer <jose.sousalves@gmail.com>
2022-02-10 21:16:09 +0000

    clickable cards

--- src/components/WorksCard.jsx
+++ src/components/WorksCard.jsx
@@ -10,36 +10,38 @@ export default function WorksCard(props) {
 
     return (
             <CardStyled className="card__container">
-                <img src={`images/works/${props.image}`} alt="" className="card__img" />
-                <div className="card__tags flex-row">
-                    <p className="card__tags--item">#{props.tags[0]}</p>
-                    <p className="card__tags--item">#{props.tags[1]}</p>
-                    <p className="card__tags--item">#{props.tags[2]}</p>
-                </div>
-                <h1 className="card__title">{props.title}</h1>
-                <p className="card__description">{props.description}</p>
-                <div className="card__btns flex-row">
-                    {props.demo && 
-                    <Btn primary>
-                        <a className="card__link" href={props.demo} target="_blank" rel="noreferrer">Demo</a>
-                    </Btn>
-                    }
-                    
-                    {props. code && 
-                    <Btn outline>
-                        <a className="card__link" href={props.code} target="_blank" rel="noreferrer">Code</a>
-                    </Btn>
-                    }
+                <a href={props.demo || props.link} target="_blank" rel="noreferrer">
+                    <img src={`images/works/${props.image}`} alt="" className="card__img" />
+                    <div className="card__tags flex-row">
+                        <p className="card__tags--item">#{props.tags[0]}</p>
+                        <p className="card__tags--item">#{props.tags[1]}</p>
+                        <p className="card__tags--item">#{props.tags[2]}</p>
+                    </div>
+                    <h1 className="card__title">{props.title}</h1>
+                    <p className="card__description">{props.description}</p>
+                    <div className="card__btns flex-row">
+                        {props.demo && 
+                        <Btn primary>
+                            <a className="card__link" href={props.demo} target="_blank" rel="noreferrer">Demo</a>
+                        </Btn>
+                        }
+                        
+                        {props. code && 
+                        <Btn outline>
+                            <a className="card__link" href={props.code} target="_blank" rel="noreferrer">Code</a>
+                        </Btn>
+                        }
 
-                    {props.link && 
-                    <Btn primary>
-                        <a className="card__link" href={props.link} target="_blank" rel="noreferrer">See More</a>
-                    </Btn>
-                    }
-                    
-                    
-                    
-                </div>
+                        {props.link && 
+                        <Btn primary>
+                            <a className="card__link" href={props.link} target="_blank" rel="noreferrer">See More</a>
+                        </Btn>
+                        }
+                        
+                        
+                        
+                    </div>
+                </a>
             </CardStyled>
         
 
