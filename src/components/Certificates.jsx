6b0bf81332f7a161444362fb676f3210fb3ce3c1
hitmaizer <jose.sousalves@gmail.com>
2022-02-10 19:50:12 +0000

    added link to each certificate

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -10,7 +10,7 @@ export default function Certificates() {
                     <img src="images/isceDouro.png" alt="" className="certificate__img" />
                     <div className="certificate__info flex-col">
                         <p className="certificate__date">Sep 2019 - Jun 2022</p>
-                        <p className="certificate__title">Interactive Multimedia Content Creation</p>
+                        <p className="certificate__title"> <a href="https://www.iscedouro.pt/pt/Oferta-formativa/1-%C2%BA-Ciclo-Licenciaturas/Producao-de-Conteudos-Interativos-e-Multimedia/" target="_blank" rel="noreferrer">Interactive Multimedia Content Creation</a></p>
                         <p className="certificate__description">University degree on creation of 
                         Interactive Multimedia content</p>
                     </div>
@@ -19,7 +19,7 @@ export default function Certificates() {
                     <img src="images/freeCode.png" alt="" className="certificate__img" />
                     <div className="certificate__info flex-col">
                         <p className="certificate__date">Sep 2020 - Feb 2021</p>
-                        <p className="certificate__title">Responsive Web Design</p>
+                        <p className="certificate__title"><a href="https://www.freecodecamp.org/certification/hitmaizer/responsive-web-design" target="_blank" rel="noreferrer">Responsive Web Design</a></p>
                         <p className="certificate__description">Developer Certification from FreeCodeCamp, 
                         representing approximately 300 hours of coursework.</p>
                     </div>
@@ -28,7 +28,7 @@ export default function Certificates() {
                     <img src="images/devchallenges.jpg" alt="" className="certificate__img" />
                     <div className="certificate__info flex-col">
                         <p className="certificate__date">Jan 2022 - Feb 2022</p>
-                        <p className="certificate__title">Responsive Web Developer</p>
+                        <p className="certificate__title"> <a href="https://devchallenges.io/certificates/FAn2CzkmrfG7oRcfTZWE" target="_blank" rel="noreferrer">Responsive Web Developer</a></p>
                         <p className="certificate__description">8 real-life like pratical challenges by devChallenges.io</p>
                     </div>
                 </div>
