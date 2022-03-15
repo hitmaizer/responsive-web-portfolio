20d6491e3cc8a5bf1cd25c35dfb502598e62baac
hitmaizer <jose.sousalves@gmail.com>
2022-03-15 20:58:55 +0000

    code cleanup

--- src/components/WorksCard.jsx
+++ src/components/WorksCard.jsx
@@ -1,50 +1,67 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
-import React from 'react';
-import CardStyled from "../elements/CardStyles"
-import Btn from "../elements/ButtonStyles"
-
+import React from "react";
+import CardStyled from "../elements/CardStyles";
+import Btn from "../elements/ButtonStyles";
 
 export default function WorksCard(props) {
-    
+  return (
+    <CardStyled className="card__container">
+      <a
+        href={props.demo || props.link}
+        target="_blank"
+        rel="noreferrer"
+        className="anchor__link"
+      >
+        <img src={`images/works/${props.image}`} alt="" className="card__img" />
+        <div className="card__tags flex-row">
+          <p className="card__tags--item">#{props.tags[0]}</p>
+          <p className="card__tags--item">#{props.tags[1]}</p>
+          <p className="card__tags--item">#{props.tags[2]}</p>
+        </div>
+        <h1 className="card__title">{props.title}</h1>
+        <p className="card__description">{props.description}</p>
+      </a>
+      <div className="card__btns flex-row">
+        {props.demo && (
+          <Btn primary>
+            <a
+              className="card__link"
+              href={props.demo}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Demo
+            </a>
+          </Btn>
+        )}
 
-    return (
-            <CardStyled className="card__container">
-                <a href={props.demo || props.link} target="_blank" rel="noreferrer" className="anchor__link">
-                    <img src={`images/works/${props.image}`} alt="" className="card__img" />
-                    <div className="card__tags flex-row">
-                        <p className="card__tags--item">#{props.tags[0]}</p>
-                        <p className="card__tags--item">#{props.tags[1]}</p>
-                        <p className="card__tags--item">#{props.tags[2]}</p>
-                    </div>
-                    <h1 className="card__title">{props.title}</h1>
-                    <p className="card__description">{props.description}</p>
-                </a>
-                    <div className="card__btns flex-row">
-                        {props.demo && 
-                        <Btn primary>
-                            <a className="card__link" href={props.demo} target="_blank" rel="noreferrer">Demo</a>
-                        </Btn>
-                        }
-                        
-                        {props. code && 
-                        <Btn outline>
-                            <a className="card__link" href={props.code} target="_blank" rel="noreferrer">Code</a>
-                        </Btn>
-                        }
+        {props.code && (
+          <Btn outline>
+            <a
+              className="card__link"
+              href={props.code}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Code
+            </a>
+          </Btn>
+        )}
 
-                        {props.link && 
-                        <Btn primary>
-                            <a className="card__link" href={props.link} target="_blank" rel="noreferrer">See More</a>
-                        </Btn>
-                        }
-                        
-                        
-                        
-                    </div>
-            </CardStyled>
-        
-
-
-    )
+        {props.link && (
+          <Btn primary>
+            <a
+              className="card__link"
+              href={props.link}
+              target="_blank"
+              rel="noreferrer"
+            >
+              See More
+            </a>
+          </Btn>
+        )}
+      </div>
+    </CardStyled>
+  );
 }
